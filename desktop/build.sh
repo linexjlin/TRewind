297bb56fad9f77fd56f727a1e92c7aa7bf08652e
linexjlin <line.xj.lin@gmail.com>
2024-07-10 10:20:52 +0800

    update

--- desktop/build.sh
+++ desktop/build.sh
@@ -1,1 +1,3 @@
-go build -ldflags="-s -w -X 'main.Version=v0.0.2'" -o TRwind core.go hide_darwin.go main.go systray.go utils.go
+#!/bin/bash
+VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+go build -ldflags="-s -w -X 'main.Version=$VERSION'" -o TRwind core.go hide_darwin.go main.go systray.go utils.go
