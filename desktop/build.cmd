6a2211c9d3eb0982918e915df72d866600b5d365
linexjlin <line.xj.lin@gmail.com>
2024-07-10 10:19:12 +0800

    update build take version from latest tag

--- desktop/build.cmd
+++ desktop/build.cmd
@@ -1,1 +1,13 @@
-go build -ldflags="-s -w -X 'main.Version=v0.1.0'"
+@echo off
+setlocal enabledelayedexpansion
+
+REM Get the last tag sorted by version
+for /f "delims=" %%i in ('git tag --sort=-v:refname ^| findstr /r /n "^" ^| findstr /b "1:"') do set "LAST_TAG=%%i"
+
+REM Remove the line number prefix if it exists
+set "LAST_TAG=%LAST_TAG:1:=%"
+
+REM Build the Go application with the version from the last tag
+go build -ldflags="-s -w -X 'main.Version=%LAST_TAG%'"
+
+endlocal
